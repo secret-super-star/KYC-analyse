7bf8c5f265edf3cb3475331abc6f90e73492e54a
secret-super-star <mjone352@gmail.com>
2020-12-07 11:22:32 +0800

    change dashbaord design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,11 +34,77 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="card card-body">
-                        <div class="col-md-12">
-                            <div id="calendar"></div>
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>150</h3>
+
+                                <p>New Orders</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-bag"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                                <p>Bounce Rate</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-stats-bars"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>44</h3>
+
+                                <p>User Registrations</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                    <div class="col-lg-3 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>65</h3>
+
+                                <p>Unique Visitors</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-pie-graph"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="card card-body">
+                            <div class="col-md-12">
+                                <div id="calendar"></div>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
                 <!-- /.row (main row) -->
@@ -60,27 +126,27 @@
     <script>
         $(function () {
             var date = new Date()
-            var d    = date.getDate(),
-                m    = date.getMonth(),
-                y    = date.getFullYear()
+            var d = date.getDate(),
+                m = date.getMonth(),
+                y = date.getFullYear()
             console.log(new Date(y, m, 1));
 
             var Calendar = FullCalendar.Calendar;
             var calendarEl = document.getElementById('calendar');
 
             var calendar = new Calendar(calendarEl, {
-                plugins: [ 'bootstrap', 'interaction', 'dayGrid', 'timeGrid' ],
-                header    : {
-                    left  : 'prev,next today',
+                plugins: ['bootstrap', 'interaction', 'dayGrid', 'timeGrid'],
+                header: {
+                    left: 'prev,next today',
                     center: 'title',
-                    right : 'dayGridMonth,timeGridWeek,timeGridDay'
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 },
                 'themeSystem': 'bootstrap',
                 //Random default events
                 events: '{{route('getusers')}}',
-                editable  : true,
-                droppable : true, // this allows things to be dropped onto the calendar !!!
-                drop      : function(info) {
+                editable: true,
+                droppable: true, // this allows things to be dropped onto the calendar !!!
+                drop: function (info) {
                     // is the "remove after drop" checkbox checked?
                     if (checkbox.checked) {
                         // if so, remove the element from the "Draggable Events" list
